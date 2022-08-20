6216c9a612bb1574cfc2b70827e6663e77e84947
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-08-20 17:17:26 +0300

    update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,64 @@
-require('pppershin.setting')
-require('pppershin.plugins')
 
-vim.cmd("colorscheme tokyonight")
+-- SETTINGS
+
+local g = vim.g
+local o = vim.o
+
+o.termguicolors = true
+
+-- Decrease update time
+o.timeoutlen = 500
+o.updatetime = 200
+
+-- Number of screen lines to keep above and below the cursor
+o.scrolloff = 8
+
+-- Better editor UI
+o.number = true
+o.numberwidth = 5
+o.relativenumber = true
+o.signcolumn = 'auto'
+o.cursorline = true
+
+-- Better editing experience
+o.expandtab = true
+o.cindent = true
+
+-- Undo and backup options
+o.backup = false
+o.writebackup = false
+o.undofile = true
+o.swapfile = false
+
+-- Remember 50 items in commandline history
+o.history = 50
+
+-- Better buffer splitting
+o.splitright = true
+o.splitbelow = true
+
+-- Preserve view while jumping
+o.jumpoptions = 'view'
+
+-- AFTER
+g.tokyonight_transparent_sidebar = true
+g.tokyonight_transparent = true
+o.background = "dark"
+vim.cmd('colorscheme tokyonight')
+
+-- PAKER
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
+  use 'wakatime/vim-wakatime'
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+  end)
+
 
