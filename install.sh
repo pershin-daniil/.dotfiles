2752b14069bf8f4888cf1cd6df1098b8d249e502
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-10-07 20:46:45 +0000

    fix: update script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-rm -frd ~/.config/{nvim|kitty|sway}
+rm -frd ~/.config/{nvim,kitty,sway}
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/kitty ~/.config/kitty
 ln -s ~/.dotfiles/sway ~/.config/sway
-
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
