83d8a1956a60e151d8dc26270dd65cef47764933
Daniil Pershin <pershin.daniil.e@gmail.com>
2025-08-29 22:45:21 +0300

    update files

--- sway/.config/sway/brightness_control.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "up" ]; then
-	light -S "$(light -G | awk '{ print int(($1 + .72) * 1.4) }')"
-fi
-
-if [ "$1" == "down" ]; then
-	light -S "$(light -G | awk '{ print int($1 / 1.4) }')"
-fi
--- sway/.config/sway/volume_control.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "up" ]; then
-	pactl set-sink-volume @DEFAULT_SINK@ +5%
-fi
-
-if [ "$1" == "down" ]; then
-	pactl set-sink-volume @DEFAULT_SINK@ -5%
-fi
